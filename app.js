abbda51eb7d673958466fece4af3880b6034bd57
craigsavage <craig.savage@ontariotechu.net>
2020-02-05 09:14:29 -0500

    Added functionality to the post route and got connection to the cloud database

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express    = require('express'),
 const Recipe = require('./models/recipe'),
       Comment = require('./models/comment');
 
-mongoose.connect('mongodb+srv://RecipeAdmin:RecipePassword@recipesonline-6rvfv.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://RecipeAdmin:RecipePassword@recipesonline-6rvfv.mongodb.net/recipes?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -20,7 +20,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('hi')
+    res.redirect('/recipes');
+});
+
+app.get('/recipes', (req, res) => {
+    Recipe.find({}, (err, found) => {
+        console.log( 'Recipes found:\n', found )
+    })
+    res.render('index');
+});
+
+// Creates a new recipe in the database then redirects to /recipes get route
+app.post('/recipes', (req, res) => {
+    // Clears database (for testing purposes)
+    Recipe.deleteMany({}, (err) => { console.log('DB cleared'); });
+
+    // if user did not provide an image url, then 'No Image Avaiable' image will be set
+    if(req.body.recipe.image === "") {
+        req.body.recipe.image = 'https://www.eberhard.com/sites/default/files/default_images/NoImage_0_0.jpg';
+    }
+
+    Recipe.create(req.body.recipe, (err, recipe) => {
+        if(err) { console.log('There was an error adding to database\n', err); }
+        else { 
+            console.log('New Recipe:', recipe);
+            res.redirect('/recipes');
+        }
+    });
 });
 
 // SERVER
--- models/recipe.js
+++ models/recipe.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 let recipeSchema = new mongoose.Schema({
     title: String,
     description: String,
-    image: String,
-    like: Number,
+    image: { type: String, default: 'https://www.eberhard.com/sites/default/files/default_images/NoImage_0_0.jpg' },
+    like: { type: Number, default: 0 },
     dateCreated: { type: Date, default: Date.now },
     comments: [
         {
